5fd64053316eb7369164573f91379b4957ab1c5c
cjogles <jackson.ogles28@gmail.com>
2020-07-06 11:09:03 -0600

    working along with instructor

--- src/04 - basic_operators.py
+++ src/04 - basic_operators.py
@@ -1,37 +1,37 @@
-"""
-Basic Operators
-"""
+# """
+# Basic Operators
+# """
 
-# 1. Addition, Subtraction, Multiplication, Division with Numbers
-number = 1 + 2 * 3 / 4.0
-print(number)
+# # 1. Addition, Subtraction, Multiplication, Division with Numbers
+# number = 1 + 2 * 3 / 4.0
+# print(number)
 
-# 2. Modulo returns integer remainder of division
-remainder = 5 % 10
-print(remainder)
+# # 2. Modulo returns integer remainder of division
+# remainder = 5 % 10
+# print(remainder)
 
-# 3. Exponentiation
-squared = 7 ** 2
-cubed = 2 ** 3
-print(squared)
-print(cubed)
+# # 3. Exponentiation
+# squared = 7 ** 2
+# cubed = 2 ** 3
+# print(squared)
+# print(cubed)
 
-# 4. Addition operator to concatenate strings
-helloworld = "hello" + " " + "world"
-print(helloworld)
+# # 4. Addition operator to concatenate strings
+# helloworld = "hello" + " " + "world"
+# print(helloworld)
 
-# 5. Multiplication to form a string with repeating sequence
-lotsofhellos = "hello" * 10
-print(lotsofhellos)
+# # 5. Multiplication to form a string with repeating sequence
+# lotsofhellos = "hello" * 10
+# print(lotsofhellos)
 
-# 6. Join lists with addition operator
-even_numbers = [2, 4, 6, 8]
-odd_numbers = [1, 3, 5, 7]
-all_numbers = odd_numbers + even_numbers
-print(all_numbers)
+# # 6. Join lists with addition operator
+# even_numbers = [2, 4, 6, 8]
+# odd_numbers = [1, 3, 5, 7]
+# all_numbers = odd_numbers + even_numbers
+# print(all_numbers)
 
-# 7. Form a new list with a repeated sequence with the multiplication operator
-print([1, 2, 3] * 3)
+# # 7. Form a new list with a repeated sequence with the multiplication operator
+# print([1, 2, 3] * 3)
 
 
 """
@@ -40,6 +40,14 @@ YOU DO
 """
 # assign 1 to `x`
 # assign 2 to `y`
+x, y = 1, 2
+x_list, y_list = ([x] * 10), ([y] * 10)
+combined = (x_list + y_list)
+print(x)
+print(y)
+print(x_list)
+print(y_list)
+print(combined)
 
 # Create two lists called `x_list` and `y_list`
 # make `x_list` contain 10 instances of `x`
